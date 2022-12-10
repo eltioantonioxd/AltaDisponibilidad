dcb2ed00960cea632c5a78b58bf75b3ca957e7be
Brayan <espinabrayan@gmail.com>
2022-12-10 00:42:45 -0300

    commit 2

--- Backend/app.py
+++ Backend/app.py
@@ -1,21 +1,27 @@
 import json
 from flask import Flask, jsonify
 import requests
-from flask_pymongo import PyMongo
 from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
 CORS(app)
-
-
-app.config["MONGO_URI"] = "mongodb://localhost:27017/chilet"
-mongo = PyMongo(app)
-
-url = 'https://www.freetogame.com/api/games'
-data = requests.get(url)
-data = data.json()
-
 app.secret_key = 'esto-es-una-clave-muy-secreta'
+
+MONGO_URI = 'mongodb://db-mongo'
+
+client = MongoClient(
+            MONGO_URI,
+            username= 'root',
+            password='brayanyandru'
+        )
+
+db = client['chilet']
+collection = db['celcius']
+
+collection.insert_one({'name': 'Teclado'})
+print('DB: ', db, flush=True)
 
 @app.route("/")
 def index():
@@ -23,7 +29,7 @@ def index():
 
 @app.route('/nombres', methods=['GET'])
 def name():
-    return jsonify({"name": "Brayan Espina Tramolao"})
+    return jsonify({"name": "Proyecto Alta Diponibilidad"})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)

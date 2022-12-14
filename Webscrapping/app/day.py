863fdfbd3d2a9a4f68a76fb3da445404cb607cd6
Andrustm <70665495+Andrustm@users.noreply.github.com>
2022-12-14 02:50:29 -0300

    ignore fix

--- Webscrapping/app/day.py
+++
@@ -1,9 +0,0 @@
-def current_date_format(date):
-    months = ("Enero", "Febrero", "Marzo", "Abri", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
-    day = date.day
-    month = months[date.month - 1]
-    year = date.year
-    messsage = "{} de {} del {}".format(day, month, year)
-
-    return messsage
-

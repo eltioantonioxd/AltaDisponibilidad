f1adb16aca3833c3a3b7ad2476541e38e887b22d
Brayan <espinabrayan@gmail.com>
2022-12-15 12:50:55 -0300

    update front

--- Backend/cliente/cliente.py
+++ Backend/cliente/cliente.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import grpc
 import json
 from google.protobuf.json_format import MessageToJson
@@ -34,32 +34,37 @@ class DataClient:
 
 @app.route('/temperature', methods=['GET'])
 def get_temperature():
-    client = DataClient()
-    response = client.get_temperature()
-    response = MessageToJson(response)
-    response = json.loads(response)
-    return jsonify(response)
+    if request.method == 'GET':
+        client = DataClient()
+        response = client.get_temperature()
+        response = MessageToJson(response)
+        response = json.loads(response)
+        return jsonify(response)
 
 @app.route('/uf', methods=['GET'])
 def get_uf():
-    client = DataClient()
-    response = client.get_uf()
-    response = MessageToJson(response)
-    response = json.loads(response)
-    return jsonify(response)
+    if request.method == 'GET':
+        client = DataClient()
+        response = client.get_uf()
+        response = MessageToJson(response)
+        response = json.loads(response)
+        return jsonify(response)
 
 @app.route('/dollar', methods=['GET'])
 def get_dollar():
-    client = DataClient()
-    response = client.get_dollar()
-    response = MessageToJson(response)
-    response = json.loads(response)
-    return jsonify(response)
+    if request.method == 'GET':
+        client = DataClient()
+        response = client.get_dollar()
+        response = MessageToJson(response)
+        response = json.loads(response)
+        return jsonify(response)
 
 @app.route('/crossword', methods=['GET'])
 def get_crossword_link():
-
-    return
+    if request.method == 'GET':
+        url = 'https://www.tarkus.info/movil/crucigrama.php?nomm=Crucigrama&nom=10/12/2022&crucigrama=8QnijP3s7QUsDDn0vzYIcZ76oBUVfYbFeXbQRd15u9Y='
+        response = {'crucigrama': url}
+    return response
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)

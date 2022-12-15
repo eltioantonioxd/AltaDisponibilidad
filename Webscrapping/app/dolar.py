54c1a5690e4a11bb73e876fc74a6e65a0516fb02
Andrustm <daille.diaz.andres@gmail.com>
2022-12-15 01:58:42 -0300

    nashe

--- Webscrapping/app/dolar.py
+++ Webscrapping/app/dolar.py
@@ -11,10 +11,10 @@ def getDolar():
     driver.get("https://www.google.com/finance/quote/USD-CLP?sa=X&ved=2ahUKEwiftpWgm_P7AhUtrpUCHfkZCQsQmY0JegQIBhAc")
     time.sleep(3)
     try:
-        dolar = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[4]/div/main/div[2]/div[1]/div[1]/c-wiz/div/div[1]/div/div[1]/div/div[1]/div/span/div/div').text
+        dollar = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[4]/div/main/div[2]/div[1]/div[1]/c-wiz/div/div[1]/div/div[1]/div/div[1]/div/span/div/div').text
         now = datetime.now()
         day = current_date_format(now)
-        data = {'dolar': dolar, 'day': day}
+        data = {'dollar': dollar, 'day': day}
         return data
     finally:
         driver.close()
